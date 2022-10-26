ce8245005c7c99bf458ddf11b0d675fbd336557b
ebadamZA <eadam@varsitycollege.co.za>
2022-10-26 22:16:26 +0200

    Add project files.

---
+++ MVCCalculator/Controllers/CalculationController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVCCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCalculator.Controllers
+{
+    public class CalculationController : Controller
+    {
+        // GET: CalculationController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: CalculationController/
+        [HttpPost]
+        public ActionResult Index(int firstValue, int secondValue, int mathOp)
+        {
+            Calculation calc = new Calculation(firstValue, secondValue, mathOp);
+            double result = calc.performCalculation();
+            ViewBag.Result = result;
+            return View();
+        }
+    }
+}
---
+++ MVCCalculator/Models/Calculation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCalculator.Models
+{
+    public enum Operation
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide
+    }
+    public class Calculation
+    {
+        public double firstValue { get; set; }
+        public double secondValue { get; set; }
+        public double mathOp { get; set; }
+
+        public Calculation(double firstValue, double secondValue, double mathOp)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.mathOp = mathOp;
+        }
+
+        public double performCalculation()
+        {
+            switch (mathOp)
+            {
+                case (int) Operation.Add:
+                    return (firstValue + secondValue);
+                case (int) Operation.Subtract:
+                    return (firstValue - secondValue);
+                case (int) Operation.Multiply:
+                    return (firstValue * secondValue);
+                default:
+                    if (secondValue != 0)
+                     return (firstValue / secondValue);
+                    else 
+                     return 0;
+            }
+        }
+    }
+}
